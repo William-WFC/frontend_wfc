6daee13d4c3bc73f1f428d3a6f56fa521a5f128b
William <1546261972@qq.com>
2016-06-19 22:14:59 +0800

    modified index.html and app.js and add ant.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-alert('Hello world!')
+
+var MessageDot = React.createClass({
+	render:function(){
+		return ({
+			<div style={{width:50px;height:50px;backgroundColor:#eee;borderRadius:6px;position:relative}}>
+				<div style={{width:10px;height:10px;backgroundColor:red;borderRadius:50%;position:absolute;right:-5px;top:-5px;}}></div>
+			</div>
+		})
+	}
+})
+
